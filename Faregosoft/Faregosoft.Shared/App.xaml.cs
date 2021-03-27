6df4b62517b71c35422299e586199dd9b3b6a8fe
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2021-03-27 15:29:01 -0500

    Login UI

--- Faregosoft/Faregosoft.Shared/App.xaml.cs
+++ Faregosoft/Faregosoft.Shared/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Faregosoft.Pages;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -85,7 +86,7 @@ namespace Faregosoft
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(LoginPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 window.Activate();
---
+++ Faregosoft/Faregosoft.Shared/Pages/LoginPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Faregosoft.Helpers;
+using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Faregosoft.Pages
+{
+    public sealed partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog messageDialog;
+
+            if (string.IsNullOrEmpty(EmailTextBox.Text))
+            {
+                messageDialog = new MessageDialog("Debes ingresar tu email.", "Error");
+                await messageDialog.ShowAsync();
+                return;
+            }
+
+            if (!RegexUtilities.IsValidEmail(EmailTextBox.Text))
+            {
+                messageDialog = new MessageDialog("Debes ingresar un email válido.", "Error");
+                await messageDialog.ShowAsync();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(PasswordPasswordBox.Password))
+            {
+                messageDialog = new MessageDialog("Debes ingresar tu contraseña.", "Error");
+                await messageDialog.ShowAsync();
+                return;
+            }
+
+            messageDialog = new MessageDialog("Vamos bien!", "OK");
+            await messageDialog.ShowAsync();
+        }
+    }
+}
